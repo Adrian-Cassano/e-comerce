28b73c6fb93c5994d86038a2090a137b7fb6e6b7
Adrian-Cassano <adriandcassano@hotmail.com>
2023-07-19 11:15:30 -0300

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./Views/Home"
+import News from "./Views/News"
+import Landing from "./Views/Landing"
 
 
 function App() {
@@ -9,6 +11,9 @@ function App() {
     <Router>
       <Routes>
         <Route path={"/"} exact element={<Home />} />
+        <Route path={"/Landing"} exact element={<Landing />} />
+        <Route path={"/News"} exact element={<News />} />
+        
       </Routes>
     </Router>
   );
