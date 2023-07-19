92541618cc41c26039e13ee49597b45e59a03e5f
Adrian-Cassano <adriandcassano@hotmail.com>
2023-07-19 11:15:55 -0300

    feat: add arrays

---
+++ src/Arrays/ModalCategoryArray.js
@@ -0,0 +1,8 @@
+const CATEGORYS =[
+    {option:"Accion", id:1},
+    {option:"Terror", id:2},
+    {option:"Carreras", id:3},
+    {option:"Deportes", id:4},
+    {option:"Disparos", id:5},
+]
+export default CATEGORYS
